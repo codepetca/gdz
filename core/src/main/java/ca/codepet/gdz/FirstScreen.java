d4e83efa4e5dfffa66aa2ad2784ff55c12caaace
stew.chan@gmail.com <stew.chan@gmail.com>
2024-11-26 10:22:41 -0500

    Add assets

--- core/src/main/java/ca/codepet/gdz/FirstScreen.java
+++ core/src/main/java/ca/codepet/gdz/FirstScreen.java
@@ -1,22 +1,50 @@
 package ca.codepet.gdz;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
-/** First screen of the application. Displayed after the application is created. */
+/**
+ * First screen of the application. Displayed after the application is created.
+ */
 public class FirstScreen implements Screen {
+    private final GameRoot game;
+    private Sprite logo;
+
+    public FirstScreen(GameRoot game) {
+        this.game = game;
+    }
+
     @Override
     public void show() {
         // Prepare your screen here.
+        Texture logoTexture = new Texture(Gdx.files.internal("textures/icon.png"));
+        logo = new Sprite(logoTexture);
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+
+        // Clear the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+
+        // Draw the logo
+        logo.draw(game.batch);
+        game.font.draw(game.batch, "Hello World!", 200, 200);
+
+        game.batch.end();
+
     }
 
     @Override
     public void resize(int width, int height) {
         // Resize your screen here. The parameters represent the new window size.
+        logo.setPosition(width / 2 - logo.getWidth() / 2, height / 2 - logo.getHeight() / 2);
+
     }
 
     @Override
--- core/src/main/java/ca/codepet/gdz/GameRoot.java
+++ core/src/main/java/ca/codepet/gdz/GameRoot.java
@@ -1,11 +1,23 @@
 package ca.codepet.gdz;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+import ca.codepet.gdz.util.FontUtil;
+
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
+ * platforms.
+ */
 public class GameRoot extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
+
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        batch = new SpriteBatch();
+        font = FontUtil.generateFont("fonts/Play-Regular.ttf", 40);
+        setScreen(new FirstScreen(GameRoot.this));
     }
 }
